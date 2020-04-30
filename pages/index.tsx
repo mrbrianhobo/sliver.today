44591c6667338ef68672f0a2cb450977262dabaa
brian <mr.brianhobo@gmail.com>
2020-04-30 01:27:56 -0700

    fetch from api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-// import fetch from 'isomorphic-unfetch';
+import fetch from 'isomorphic-unfetch';
 import { useState } from 'react';
 import { NextPage } from 'next';
 import { Flex } from 'theme-ui';
@@ -56,10 +56,10 @@ const IndexPage: NextPage<Props> = ({ pizzas }) => {
 }
 
 IndexPage.getInitialProps = async () => {
-  // const res = await fetch('https://api.sliver.today/');
-  // const json = await res.json();
+  const res = await fetch('https://api.sliver.today/');
+  const json = await res.json();
 
-  const json = JSON.parse(`{"pizzas":[{"location":"telegraph","date":"2020-04-29","pizza":"Wild Mushrooms (Shitake, Chanterelle, Portabella, Cremini Mushrooms), Mozzarella, Asiago Fresh, Green Scallons, Fresh Herbs, Infused Chanterelle Mushrooms, and Garlic Olive Oil."},{"location":"shattuck","date":"2020-04-29","pizza":"Roasted Yukon Gold Potatoes, Baby Spinach Mixed Caramelized Onions, Mozzarella, Aged Sharp Cheddar Cheese, Fresh Herbs, Infused Thyme Garlic Olive Oil."},{"location":"broadway","date":"2020-04-29","pizza":"Assorted Organic heirloom Tomatoes, Cremini Mushrooms, Mozzarella, French Feta Cheese. Topped With Avocado Spread"}]}`);
+  // const json = JSON.parse(`{"pizzas":[{"location":"telegraph","date":"2020-04-29","pizza":"Wild Mushrooms (Shitake, Chanterelle, Portabella, Cremini Mushrooms), Mozzarella, Asiago Fresh, Green Scallons, Fresh Herbs, Infused Chanterelle Mushrooms, and Garlic Olive Oil."},{"location":"shattuck","date":"2020-04-29","pizza":"Roasted Yukon Gold Potatoes, Baby Spinach Mixed Caramelized Onions, Mozzarella, Aged Sharp Cheddar Cheese, Fresh Herbs, Infused Thyme Garlic Olive Oil."},{"location":"broadway","date":"2020-04-29","pizza":"Assorted Organic heirloom Tomatoes, Cremini Mushrooms, Mozzarella, French Feta Cheese. Topped With Avocado Spread"}]}`);
 
   const telegraphMenu = json.pizzas[0].pizza;
   const shattuckMenu = json.pizzas[1].pizza;
